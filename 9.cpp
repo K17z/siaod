cb3c1080b8dda9127c0131767d983eab74fd5063
K17z <100905750+K17z@users.noreply.github.com>
2022-04-15 10:14:06 +0300

    Add files via upload

--- 9.cpp
+++ 9.cpp
@@ -1,57 +1,74 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include <stdlib.h>
-#define MAXWORD 100
-struct tnode {
-    char* word;
-    int count;
-    struct tnode* left;
-    struct tnode* right;
-};
-struct tnode* addtree(struct tnode* p, char* w) {
-    int cond;
-    if (p == NULL) {
-        p = (struct tnode*)malloc(sizeof(struct tnode));
-        p->word = _strdup(w);
-        p->count = 1;
-        p->left = p->right = NULL;
+#include "iostream"
+#include "windows.h"
+
+int max_razr(int chislo)
+{
+    int max = 0;
+    while (chislo > 1)
+    {
+        chislo /= 10;
+        max++;
     }
-    else if ((cond = strcmp(w, p->word)) == 0)
-        p->count++;
-    else if (cond < 0)
-        p->left = addtree(p->left, w);
-    else
-        p->right = addtree(p->right, w);
-    return p;
+    return max;
 }
-void freemem(tnode* tree) {
-    if (tree != NULL) {
-        freemem(tree->left);
-        freemem(tree->right);
-        free(tree->word);
-        free(tree);
+
+int velich_razr(int chislo, int razr)
+{
+    while (razr > 1)
+    {
+        chislo /= 10;
+        razr--;
+    }
+    return chislo % 10;
+}
+
+void sort_razr(int** dop_mas, int* mas, int razr,int n)
+{
+    int* mas_col, i, j, temp = 0;
+    mas_col = new int[n];
+    for (i = 0; i < n; i++)
+        mas_col[i] = 0;
+    for (i = 0; i < n; i++)
+    {
+        int a = velich_razr(mas[i], razr);
+        dop_mas[mas_col[a]][a] = mas[i];
+        mas_col[a]++;
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < mas_col[i]; j++)
+        {
+            mas[temp] = dop_mas[j][i];
+            temp++;
+        }
     }
 }
-void treeprint(struct tnode* p) {
-    if (p != NULL) {
-        treeprint(p->left);
-        printf("%d %s\n", p->count, p->word);
-        treeprint(p->right);
+
+int main()
+{
+    using namespace std;
+    int n, col_razr = 0;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int razr, i, * mas, ** dop_mas;
+    cout << "Введите размер последовательности: ";
+    cin >> n;
+    mas = new int[n];
+    cout << "Введите элементы последовательности: " << endl;
+    for (int i = 0; i < n; i++) //Рандом заполнение массива
+    {
+        mas[i] = rand() % 100; // заполняем массив случайными числами
+        cout << "[" << i + 1 << "]="<<mas[i]<<endl;
     }
-}
-int main() {
-    struct tnode* root;
-    char word[MAXWORD];
-    root = NULL;
-    do {
-        scanf_s("%s", word, MAXWORD);
-        if (isalpha(word[0]))
-            root = addtree(root, word);
-    } while (word[0] != '0');
-    treeprint(root);
-    freemem(root);
-    getchar();
-    getchar();
+    dop_mas = new int* [n];
+    for (i = 0; i < n; i++)
+        dop_mas[i] = new int[n];
+    for (i = 0; i < n; i++)
+        if (col_razr < max_razr(mas[i]))
+            col_razr = max_razr(mas[i]);
+    for (razr = 1; razr <= col_razr; razr++)
+        sort_razr(dop_mas, mas, razr,n);
+    for (i = 0; i < n; i++)
+        cout << mas[i] << endl;
     return 0;
 }
